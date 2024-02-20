5e079f0f376fcc021ef256e0db99ad3c40794355
Minjun Kim <damon911@naver.com>
2024-02-20 20:10:56 +0900

    Jetpack Compose 응용

--- ComposeTest/app/build.gradle.kts
+++ ComposeTest/app/build.gradle.kts
@@ -50,6 +50,7 @@ android {
 }
 
 dependencies {
+    implementation("androidx.compose.material:material-icons-extended")
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
