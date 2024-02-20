9e54da5eeb76963bfd104509761c8a97d6e71e14
Minjun Kim <damon911@naver.com>
2024-02-20 19:18:28 +0900

    Jetpack Compose 컴포저블 재사용

--- ComposeTest/app/src/main/java/mj/kotlintest/composetest/MainActivity.kt
+++ ComposeTest/app/src/main/java/mj/kotlintest/composetest/MainActivity.kt
@@ -19,15 +19,19 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeTestTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                MyApp(modifier = Modifier.fillMaxSize())
             }
         }
+    }
+}
+
+@Composable
+fun MyApp(modifier: Modifier = Modifier) {
+    Surface(
+        modifier = modifier,
+        color = MaterialTheme.colorScheme.background
+    ) {
+        Greeting("Android")
     }
 }
 
@@ -45,6 +49,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     ComposeTestTheme {
-        Greeting("Android")
+        MyApp()
     }
 }
